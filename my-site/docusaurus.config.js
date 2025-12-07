42649db754bc555b5eaf7128719911236d4a4976
Anoushey-chandio <161858128+Anoushey-chandio@users.noreply.github.com>
2025-12-06 16:49:23 -0800

    Update docusaurus.config.js

--- my-site/docusaurus.config.js
+++ my-site/docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   // Set the production url of your site here
   url: 'https://Anoushey-chandio.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  baseUrl: '/physical-ai-book/', // Base URL in your GitHub Pages URL
+ baseUrl: '/ai-book-1/', // Base URL in your GitHub Pages URL
 
   // GitHub pages deployment config
   organizationName: 'Anoushey-chandio', // GitHub username
